9e1d2a739025400c3a174a3ff3f808a9b4072e0d
fernandoG494 <lfgc851@gmail.com>
2022-10-20 19:11:46 -0500

    Initial commit with basic counter

---
+++ 01-bases/src/App.tsx
@@ -0,0 +1,11 @@
+import Counter from "./bases/Counter";
+
+function App() {
+    return (
+        <>
+        <Counter initialState={14}/>
+        </>
+    );
+}
+
+export default App;
---
+++ 01-bases/src/bases/Counter.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+const Counter = ({ initialState = 0 }) => {
+    const [counter, setCounter] = useState(initialState);
+    
+    const handleClick = () => {
+        setCounter(prev => prev + 1);
+    };
+
+    return (
+        <>
+            <h1>Counter: { counter }</h1>
+            <button onClick={handleClick}>
+                +1
+            </button>
+        </>
+    )
+}
+
+export default Counter
