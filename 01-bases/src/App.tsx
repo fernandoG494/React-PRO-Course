492303e81fdcab48fc2b83ac3e48f5a73bcd2350
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2022-10-25 16:29:09 -0500

    bases implemented

--- 01-bases/src/App.tsx
+++ 01-bases/src/App.tsx
@@ -2,7 +2,8 @@ import Counter from "./bases/Counter";
 import CounterBy from "./bases/CounterBy";
 import CounterEffect from "./bases/CounterEffect";
 import { CounterHook } from "./bases/CounterHook";
-import CounterReducer from "./bases/CounterReducer";
+// import CounterReducer from "./bases/CounterReducer";
+import CounterReducer from './reducers/CounterReducer';
 
 function App() {
     return (
