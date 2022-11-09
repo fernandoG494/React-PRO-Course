cf9eefb12aaa9a606a2b2c35b496b2d1c8733686
fernandoG494 <lfgc851@gmail.com>
2022-11-09 10:21:12 -0600

    dinamic routes implemented

--- 02-react-app/src/01-lazyload/layout/LazyLayout.tsx
+++ 02-react-app/src/01-lazyload/layout/LazyLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route, Routes, Navigate } from 'react-router-dom';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../pages';
 
 export const LazyLayout = () => {
     return (
@@ -17,6 +18,14 @@ export const LazyLayout = () => {
                     <NavLink to='lazy3'>Lazy 3</NavLink>
                 </li>
             </ul>
+
+            <Routes>
+                <Route path='lazy1' element={<LazyPage1 />} />
+                <Route path='lazy2' element={<LazyPage2 />} />
+                <Route path='lazy3' element={<LazyPage3 />} />
+
+                <Route path='*' element={ <Navigate replace to='lazy1' /> } />
+            </Routes>
         </div>
     )
 }
--- 02-react-app/src/Routes/routes.ts
+++ 02-react-app/src/Routes/routes.ts
@@ -11,16 +11,13 @@ interface Route {
 };
 
 const LazyLayout = lazy(() => import(/* webpackChunkName: "LazyPage1" */'../01-lazyload/layout/LazyLayout'));
-// const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */'../01-lazyload/pages/LazyPage2'));
-// const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */'../01-lazyload/pages/LazyPage3'));
-
 
 export const routes: Route[] = [
     {
         to: '/lazyload/',
-        path: '/lazyload/',
+        path: '/lazyload/*',
         Component: LazyLayout,
-        name: 'Lazy layout'
+        name: 'LazyLayout'
     },
     {
         to: '/no-lazy',
