10700a6da9d48ed0ed7b427af61b36e580a1e8fc
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2022-11-24 20:59:59 -0600

    CSS dinamyc values

--- 02-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 02-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -1,28 +1,32 @@
 import { useProduct } from "../hooks/useProduct";
-import { createContext } from "react";
+import { createContext, CSSProperties, ReactElement } from "react";
 import styles from "../styles/styles.module.css";
-import {
-  ProductContextProps,
-  ProductCardProps,
-} from "../interfaces/interfaces";
+import { Product, ProductContextProps } from "../interfaces/interfaces";
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ children, product }: ProductCardProps) => {
+export interface Props {
+  product: Product;
+  children?: ReactElement | ReactElement[];
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const ProductCard = ({ children, product, className, style }: Props) => {
   const { counter, increaseBy } = useProduct();
 
   return (
-    <div className={styles.productCard}>
-      <Provider
-        value={{
-          counter,
-          increaseBy,
-          product,
-        }}
-      >
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={`${styles.productCard} ${className}`} style={style}>
         {children}
-      </Provider>
-    </div>
+      </div>
+    </Provider>
   );
 };
--- 02-react-app/src/02-component-patterns/components/index.ts
+++ 02-react-app/src/02-component-patterns/components/index.ts
@@ -1,9 +1,9 @@
 import { ProductCard as ProductCardHOC } from "./ProductCard";
+import { ProductCardHOCProps } from "../interfaces/interfaces";
 
 import { ProductButtons } from "./ProductButtons";
 import { ProductImage } from "./ProductImage";
 import { ProductTitle } from "./ProductTitle";
-import { ProductCardHOCProps } from "../interfaces/interfaces";
 
 export { ProductButtons } from "./ProductButtons";
 export { ProductImage } from "./ProductImage";
